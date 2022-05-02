51e0f92f9fdc513f5541a30d3375fdce68f93b00
Cristina Morillo <cmoril01@ucm.es>
2022-05-02 22:47:27 +0200

    Id Conjunto Visitante

--- src/model/ConjuntoVisitante.java
+++ src/model/ConjuntoVisitante.java
@@ -4,7 +4,7 @@ import javax.swing.ImageIcon;
 
 public class ConjuntoVisitante extends Conjunto {
 	
-	public static final String id = "ConVisit";
+	public static final String id = "ConVisitante";
 
 	public ConjuntoVisitante(double precio, boolean activo, int stock, int udsvendidas, int reservados, String talla, String nombre, int dorsal) {
 		super(id,precio, activo, stock, udsvendidas, reservados);
