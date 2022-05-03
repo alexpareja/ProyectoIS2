f517d6126f23e90fae120a9973151d44a35c949a
mbasca <100694317+mbasca@users.noreply.github.com>
2022-05-03 11:42:10 +0200

    Comentarios y limpieza paquete vista (1/2))

--- src/Viewer/ArticuloPanel.java
+++ src/Viewer/ArticuloPanel.java
@@ -11,6 +11,7 @@ import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
@@ -19,129 +20,113 @@ import controller.Controller;
 import model.InventarioObserver;
 import model.Producto;
 
-public class ArticuloPanel extends JPanel implements InventarioObserver{
-	
+public class ArticuloPanel extends JPanel implements InventarioObserver {
+
 	private static final long serialVersionUID = 1L;
 	private Producto _prod;
 	private Controller _ctrl;
-
+	
 	public ArticuloPanel(Controller ctrl, Producto prod) {
 		_prod = prod;
 		initGUI();
 		_ctrl = ctrl;
 		_ctrl.addObserver(this);
 	}
-	
-	private void initGUI() {
-		setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
 
+//Se inicia la interfaz mediante un BoxLayout
+	private void initGUI() {
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+//Se cogen el precio, el nombre y la imagen 
 		String precio = Double.toString(_prod.getPrecio());
 		JLabel titulo = new JLabel(_prod.getId());
 		JLabel precioL = new JLabel(precio + " euros");
-	
+
 		JLabel imagen = new JLabel(" ");
 		imagen.setIcon(_prod.getFoto());
 		imagen.setVisible(true);
-		
+
 		titulo.setAlignmentX(CENTER_ALIGNMENT);
 		precioL.setAlignmentX(CENTER_ALIGNMENT);
 		imagen.setAlignmentX(CENTER_ALIGNMENT);
 		
+//Se añaden al panel
 		add(titulo);
 		add(imagen);
-		
+//Se coge el resto de información necesaria para mostrar y se añade al panel
 		String[] info = _prod.mostrar();
-		JPanel panelInfo = new JPanel(new GridLayout(0,2));
-		for(int i = 0; i < info.length;i++) {
-				JLabel a = new JLabel (info[i]);
-				a.setHorizontalAlignment(JLabel.CENTER);
-				panelInfo.add(a);	
+		JPanel panelInfo = new JPanel(new GridLayout(0, 2));
+		for (int i = 0; i < info.length; i++) {
+			JLabel a = new JLabel(info[i]);
+			a.setHorizontalAlignment(JLabel.CENTER);
+			panelInfo.add(a);
 		}
 		add(panelInfo);
-		
-		/*
-		JComboBox<String> tll = new JComboBox<String>();
-		String[] tallas= {"XS" ,"S", "M", "L", "XL"}; //Hacer tallas disponibles en cada articulo
-		for(int i = 0; i < tallas.length; i++) {
-			tll.addItem(tallas[i]);
-		}
-		
-		JComboBox<String> nombr = new JComboBox<String>();
-		String[] nombres= {"Alaba" ,"Marcelo", "CR7", "Benzema", "Ansu Fati"}; //Hacer nombres disponibles en cada articulo
-		for(int i = 0; i < nombres.length; i++) {
-			nombr.addItem(nombres[i]);
-		}
-		
-		
-		JPanel opt = new JPanel(new GridLayout(1, 2));
-		
-		opt.add(tll);
-		opt.add(nombr);
-		
-		
-		this.add(opt);
-		*/
-		
-	
+
 		JPanel options = new JPanel();
 		options.setLayout(new BoxLayout(options, BoxLayout.LINE_AXIS));
 		Border raisedBorder = BorderFactory.createRaisedBevelBorder();
 		
+    //Se añade el boton para meter el articulo al carrito
 		JButton comprar = new JButton("Anadir al carrito");
-		comprar.setMinimumSize(new Dimension(150,20));
-		comprar.setMaximumSize(new Dimension(150,20));
-		comprar.setPreferredSize(new Dimension(150,20));
-		comprar.addActionListener(new ActionListener(){  
-			public void actionPerformed(ActionEvent e){
+		comprar.setMinimumSize(new Dimension(150, 20));
+		comprar.setMaximumSize(new Dimension(150, 20));
+		comprar.setPreferredSize(new Dimension(150, 20));
+		comprar.addActionListener(new ActionListener() {
+			//cuando se pulsa se añade al carrito
+			public void actionPerformed(ActionEvent e) {
 				_ctrl.getC().anadirCarrito(_prod);
+				JOptionPane.showMessageDialog(null, "Producto anadido al carrito", "Confirmacion",
+						JOptionPane.INFORMATION_MESSAGE);
 			}
 		});
 		comprar.setAlignmentX(LEFT_ALIGNMENT);
 		comprar.setBorder(raisedBorder);
 		comprar.setFocusable(false);
 		options.add(comprar);
-		
+       //Se añade el boton para reservar
 		JButton reservar = new JButton("Reservar");
-		reservar.setMinimumSize(new Dimension(100,20));
-		reservar.setMaximumSize(new Dimension(100,20));
-		reservar.setPreferredSize(new Dimension(100,20));
-		reservar.addActionListener(new ActionListener(){  
-			public void actionPerformed(ActionEvent e){
+		reservar.setMinimumSize(new Dimension(100, 20));
+		reservar.setMaximumSize(new Dimension(100, 20));
+		reservar.setPreferredSize(new Dimension(100, 20));
+		reservar.addActionListener(new ActionListener() {
+			//Cuando se pulsa se reserva el producto
+			public void actionPerformed(ActionEvent e) {
 				_ctrl.getI().reservar(_prod);
+				JOptionPane.showMessageDialog(null, "Producto reservado", "Confirmacion",
+						JOptionPane.INFORMATION_MESSAGE);
 			}
 		});
 		reservar.setAlignmentX(RIGHT_ALIGNMENT);
 		reservar.setBorder(raisedBorder);
 		reservar.setFocusable(false);
 		options.add(reservar);
-		
+
 		add(options);
-		
-		setMinimumSize(new Dimension(100,200));
-		setMaximumSize(new Dimension(200,200));
-		setPreferredSize(new Dimension(150,200));
-		setBorder(BorderFactory.createTitledBorder(
-				BorderFactory.createLineBorder(Color.black, 2),null ,TitledBorder.LEFT, TitledBorder.TOP));
-	
+
+		setMinimumSize(new Dimension(100, 200));
+		setMaximumSize(new Dimension(200, 200));
+		setPreferredSize(new Dimension(150, 200));
+		setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 2), null,
+				TitledBorder.LEFT, TitledBorder.TOP));
+
 	}
-	
 
 	@Override
 	public void onRegistroTienda(ArrayList<Producto> inventario) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onActualizaTienda(ArrayList<Producto> inventario) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onCambiarPrecio(ArrayList<Producto> inventario) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- src/Viewer/BarraInferior.java
+++ src/Viewer/BarraInferior.java
@@ -14,53 +14,58 @@ import model.Producto;
 
 public class BarraInferior extends JPanel implements InventarioObserver {
 	private static final long serialVersionUID = 1L;
-	private JButton izq = new JButton(); 
-	private JButton dcha  = new JButton(); 
+	private JButton izq = new JButton();
+	private JButton dcha = new JButton();
 	private Controller _ctrl;
-	
+
 	BarraInferior(Controller ctrl) {
 		initGUI();
 		ctrl.addObserver(this);
 		_ctrl = ctrl;
 	}
+
+	// Se incia la interfaz con un BorderLayout y se añaden los botones de las
+	// flechas
 	private void initGUI() {
-		this.setLayout( new BorderLayout());
+		this.setLayout(new BorderLayout());
 		izq.setText("<=");
 		dcha.setText("=>");
 		izq.setFocusable(false);
 		dcha.setFocusable(false);
-		izq.addActionListener(new ActionListener(){  
-			public void actionPerformed(ActionEvent e){		
+		// Si se pulsa el boton izq se pasa pagina a la izquierda
+		izq.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				_ctrl.getT().desplazaIzq();
-				
+
 			}
 		});
-		
-		dcha.addActionListener(new ActionListener(){  
-			public void actionPerformed(ActionEvent e){		
+		// Si se pulsa el boton dcha se pasa pagina a la derecha
+		dcha.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				_ctrl.getT().desplazaDer();
 			}
 		});
-		
+
 		this.add(izq, BorderLayout.WEST);
 		this.add(dcha, BorderLayout.EAST);
 	}
-	
+
 	@Override
 	public void onRegistroTienda(ArrayList<Producto> inventario) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void onActualizaTienda(ArrayList<Producto> inventario) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void onCambiarPrecio(ArrayList<Producto> inventario) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
-
