aa1a44fad366e400ac26a42e632797a406f238b0
alexpareja <aparej01@ucm.es>
2022-05-03 10:10:41 +0200

    Refactorizar codigo (2)

--- src/Viewer/ArticuloPanel.java
+++ src/Viewer/ArticuloPanel.java
@@ -19,8 +19,8 @@ import controller.Controller;
 import model.InventarioObserver;
 import model.Producto;
 
-public class ArticuloPanel extends JPanel implements InventarioObserver{
-	
+public class ArticuloPanel extends JPanel implements InventarioObserver {
+
 	private static final long serialVersionUID = 1L;
 	private Producto _prod;
 	private Controller _ctrl;
@@ -31,68 +31,44 @@ public class ArticuloPanel extends JPanel implements InventarioObserver{
 		_ctrl = ctrl;
 		_ctrl.addObserver(this);
 	}
-	
+
 	private void initGUI() {
-		setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
 		String precio = Double.toString(_prod.getPrecio());
 		JLabel titulo = new JLabel(_prod.getId());
 		JLabel precioL = new JLabel(precio + " euros");
-	
+
 		JLabel imagen = new JLabel(" ");
 		imagen.setIcon(_prod.getFoto());
 		imagen.setVisible(true);
-		
+
 		titulo.setAlignmentX(CENTER_ALIGNMENT);
 		precioL.setAlignmentX(CENTER_ALIGNMENT);
 		imagen.setAlignmentX(CENTER_ALIGNMENT);
-		
+
 		add(titulo);
 		add(imagen);
-		
+
 		String[] info = _prod.mostrar();
-		JPanel panelInfo = new JPanel(new GridLayout(0,2));
-		for(int i = 0; i < info.length;i++) {
-				JLabel a = new JLabel (info[i]);
-				a.setHorizontalAlignment(JLabel.CENTER);
-				panelInfo.add(a);	
+		JPanel panelInfo = new JPanel(new GridLayout(0, 2));
+		for (int i = 0; i < info.length; i++) {
+			JLabel a = new JLabel(info[i]);
+			a.setHorizontalAlignment(JLabel.CENTER);
+			panelInfo.add(a);
 		}
 		add(panelInfo);
-		
-		/*
-		JComboBox<String> tll = new JComboBox<String>();
-		String[] tallas= {"XS" ,"S", "M", "L", "XL"}; //Hacer tallas disponibles en cada articulo
-		for(int i = 0; i < tallas.length; i++) {
-			tll.addItem(tallas[i]);
-		}
-		
-		JComboBox<String> nombr = new JComboBox<String>();
-		String[] nombres= {"Alaba" ,"Marcelo", "CR7", "Benzema", "Ansu Fati"}; //Hacer nombres disponibles en cada articulo
-		for(int i = 0; i < nombres.length; i++) {
-			nombr.addItem(nombres[i]);
-		}
-		
-		
-		JPanel opt = new JPanel(new GridLayout(1, 2));
-		
-		opt.add(tll);
-		opt.add(nombr);
-		
-		
-		this.add(opt);
-		*/
-		
-	
+
 		JPanel options = new JPanel();
 		options.setLayout(new BoxLayout(options, BoxLayout.LINE_AXIS));
 		Border raisedBorder = BorderFactory.createRaisedBevelBorder();
-		
+
 		JButton comprar = new JButton("Anadir al carrito");
-		comprar.setMinimumSize(new Dimension(150,20));
-		comprar.setMaximumSize(new Dimension(150,20));
-		comprar.setPreferredSize(new Dimension(150,20));
-		comprar.addActionListener(new ActionListener(){  
-			public void actionPerformed(ActionEvent e){
+		comprar.setMinimumSize(new Dimension(150, 20));
+		comprar.setMaximumSize(new Dimension(150, 20));
+		comprar.setPreferredSize(new Dimension(150, 20));
+		comprar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				_ctrl.getC().anadirCarrito(_prod);
 			}
 		});
@@ -100,13 +76,13 @@ public class ArticuloPanel extends JPanel implements InventarioObserver{
 		comprar.setBorder(raisedBorder);
 		comprar.setFocusable(false);
 		options.add(comprar);
-		
+
 		JButton reservar = new JButton("Reservar");
-		reservar.setMinimumSize(new Dimension(100,20));
-		reservar.setMaximumSize(new Dimension(100,20));
-		reservar.setPreferredSize(new Dimension(100,20));
-		reservar.addActionListener(new ActionListener(){  
-			public void actionPerformed(ActionEvent e){
+		reservar.setMinimumSize(new Dimension(100, 20));
+		reservar.setMaximumSize(new Dimension(100, 20));
+		reservar.setPreferredSize(new Dimension(100, 20));
+		reservar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				_ctrl.getI().reservar(_prod);
 			}
 		});
@@ -114,28 +90,27 @@ public class ArticuloPanel extends JPanel implements InventarioObserver{
 		reservar.setBorder(raisedBorder);
 		reservar.setFocusable(false);
 		options.add(reservar);
-		
+
 		add(options);
-		
-		setMinimumSize(new Dimension(100,200));
-		setMaximumSize(new Dimension(200,200));
-		setPreferredSize(new Dimension(150,200));
-		setBorder(BorderFactory.createTitledBorder(
-				BorderFactory.createLineBorder(Color.black, 2),null ,TitledBorder.LEFT, TitledBorder.TOP));
-	
+
+		setMinimumSize(new Dimension(100, 200));
+		setMaximumSize(new Dimension(200, 200));
+		setPreferredSize(new Dimension(150, 200));
+		setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 2), null,
+				TitledBorder.LEFT, TitledBorder.TOP));
+
 	}
-	
 
 	@Override
 	public void onRegistroTienda(ArrayList<Producto> inventario) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onActualizaTienda(ArrayList<Producto> inventario) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -14,36 +14,34 @@ public class Controller {
 	private Sesion s;
 
 	private Carrito c;
-	
+
 	public Controller(Inventario inv, Carrito carr, Tienda tiend) {
-		this.i=inv;
+		this.i = inv;
 		this.c = carr;
 		this.t = tiend;
-		this.s=new Sesion();
+		this.s = new Sesion();
 	}
-	
+
 	public boolean comprar(Carrito carrito) {
 		return i.comprar(carrito);
 	}
-	
+
 	public void addObserver(InventarioObserver o) {
 		i.addObserver(o);
 		t.addObserver(o);
 	}
-	
+
 	public void actualizarTienda(InventarioObserver o) {
 		i.actualizarTienda();
 	}
 
-
 	public Carrito getC() {
 		return c;
 	}
-	
+
 	public Inventario getI() {
 		return i;
 	}
-
 
 	public Tienda getT() {
 		return t;
@@ -55,7 +53,7 @@ public class Controller {
 
 	public void addObserverUsuario(UsuariosObserver o) {
 		s.addObserver(o);
-		
-	}	
-	
+
+	}
+
 }
